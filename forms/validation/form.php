b683be5bff55a8c2bb981144051e1425b40178b4
JaViKo500 <javikogutierrez64@gmail.com>
2026-06-18 22:03:51 -0500

    feat(forms): add form validation example with input sanitization

---
+++ forms/validation/form.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Validation</title>
+</head>
+<body>
+
+  <?php
+    $name = $email = $website = $comment = $gender = "";
+    if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+      $name = testInput($_POST['name'] ?? 'Not specified');
+      $email = testInput($_POST['email'] ?? 'Not specified');
+      $website = testInput($_POST['website'] ?? 'Not specified');
+      $comment = testInput($_POST['comment'] ?? 'Not specified');
+      $gender = testInput($_POST['gender'] ?? 'Not specified');
+    }
+
+    function testInput( string $value ) : string {
+      $value = trim($value);
+      $data = stripcslashes($value);
+      return htmlspecialchars($data);
+    }
+  ?>
+
+  <h2>PHP Form Validation Example</h2>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+    Name: <input type="text" name="name" required>
+    <br><br>
+    E-mail: <input type="text" name="email" required>
+    <br><br>
+    Website: <input type="text" name="website">
+    <br><br>
+    Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+    <br><br>
+    Gender:
+    <input type="radio" name="gender" value="female">Female
+    <input type="radio" name="gender" value="male">Male
+    <input type="radio" name="gender" value="other">Other
+    <br><br>
+    <input type="submit" name="submit" value="Submit">  
+  </form>
+
+  <?php
+    if ( !empty($name) ) {
+      echo '<div style="border: 1px solid black; padding: 10px; margin: 10px;">';
+        echo "<h3>Name</h3>";
+        echo $name;
+        echo "<h3>E-mail</h3>";
+        echo $email;
+        echo "<h3>Website</h3>";
+        echo $website;
+        echo "<h3>Comment</h3>";
+        echo $comment;
+        echo "<h3>Gender</h3>";
+        echo $gender; 
+      echo "</div>";
+    }
+  ?>
+</body>
+</html>
